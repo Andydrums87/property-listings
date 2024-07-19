7347fee4333bd29d6438c50f4ac4fa2031f2dcf7
Andy Joseph <neon_4@hotmail.com>
2024-07-19 15:56:47 +0100

    update image path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header () {
                 <h1 id="header__title">Peace, nature, <br /> dream</h1>
                 <p id="header__para">Find and book a great experience</p>
             </div>
-         <img className="hero__img"src={Image} alt="Hero Image" />
+         <img className="hero__img"src="https://andydrums87.github.io/property-listings/assets/hero-image-2pvHLmLd.jpg" alt="Hero Image" />
         </div>
     )
 }
